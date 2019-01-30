ebd3a36c82b7f079a392dc147cd04f50cc04f0f0
Courtney Tyler <courtneytyler7@gmail.com>
2019-01-30 14:31:36 -0500

    successful seeding

--- controllers/application.js
+++ controllers/application.js
@@ -3,7 +3,6 @@ module.exports = {
     index: (req, res) => {
       Concert.find({})
       .sort({ createdAt: -1 })
-      .limit(10)
       .populate("author")
       .then(concerts => {
         res.render("app/index", { concerts });
--- db/seed.js
+++ db/seed.js
@@ -9,21 +9,30 @@ User.find({}).remove(() => {
   Concert.find({}).remove(() => {
     let bugs = User.create({
         email: "bugsbunny@gmail.com",
-        password: "bugsbunny"
+        password: "bugsbunny",
+        username: "bugs"
     }).then(user => {
         Promise.all([
           Concert.create({
-            content: "eh, what's up doc?",
+            artistOrArtists: "a1",
+            url: "u1",
+            noteworthy: "n1",
+            photos: "p1",
+            datedAttended: "11/11/11",
             author: user._id
           }).then(concert => {
             user.concerts.push(concert)
           }),
           Concert.create({
-            content: "That's all, folks!",
+            artistOrArtists: "a2",
+            url: "u2",
+            noteworthy: "n2",
+            photos: "p2",
+            datedAttended: "11/11/11",
             author: user._id
           }).then(concert => {
             user.concerts.push(concert)
-          })
+          }),
         ]).then(() => {
           user.save(err => console.log(err))
         })
