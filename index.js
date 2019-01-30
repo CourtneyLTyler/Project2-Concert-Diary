896e09c27723e67f7d3c1bc33d1f031a795fbbc4
Courtney Tyler <courtneytyler7@gmail.com>
2019-01-30 00:50:18 -0500

    rearrange root

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const express = require('express')
-const app = express()
-const bodyParser = require("body-parser")
+const express = require("express");
+const app = express();
 const hbs = require("hbs");
 const flash = require("connect-flash");
-const methodOverride = require("method-override")
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 const session = require('express-session')
 const passport = require('passport')
-const cookieParser = require("cookie-parser");
 
-app.set('view engine', 'hbs')
-
-app.use(flash())
+app.use(flash());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-    session({
-      secret: "EXPRESS-IS-AWESOME",
-      saveUninitialized: true,
-      resave: false
-    })
-  );
+  session({
+    secret: "EXPRESS-IS-AWESOME",
+    saveUninitialized: true,
+    resave: false
+  })
+);
 
-require('./config/passport')(passport)
-app.use(passport.initialize())
-app.use(passport.session())
+
+require("./config/passport")(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static("public"));
+app.set("view engine", "hbs");
 app.use(methodOverride("_method"));
 
-app.use(require("./routes/index.js"))
 
-app.listen(4000, () => {
-    console.log('app is listening on port 4000')
-})
 
+app.use(require("./routes/index.js"));
+
+app.listen(4000, () => console.log("server is running"));
 
