28e7d7cdbe432625a59a9f19622f905c6c211249
Courtney Tyler <courtneytyler7@gmail.com>
2019-01-30 00:47:19 -0500

    cleanup root file

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const passport = require('passport')
 // const userController = require("../controllers/user.js");
 
 
-require('./config/passport')(passport)
-app.use(passport.initialize())
-app.use(passport.session())
+// require('./config/passport')(passport)
+// app.use(passport.initialize())
+// app.use(passport.session())
 
 
 
@@ -46,20 +46,3 @@ app.use(require("./routes/index.js"));
 
 app.listen(4000, () => console.log("server is running"));
 
-
-// const express = require("express");
-// const hbs = require("hbs");
-// const bodyParser = require("body-parser");
-// // const methodOverride = require("method-override");
-
-// const app = express();
-// hbs.registerPartials(__dirname + "/views/partials"); 
-// // how we serve css files w/in Express
-// app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.set("view engine", "hbs");
-// // app.use(methodOverride("_method"));
-
-// app.use(require("./routes/index.js"));
-
-// app.listen(4000, () => console.log("4000 server is running"));
